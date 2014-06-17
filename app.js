f74470a2d65a568b7e249ec83136d509d2e1b65b
Amila Iddamalgoda <amila922@gmail.com>
2014-06-17 19:18:26 +0530

    first commit

--- app.js
+++ app.js
@@ -3,14 +3,19 @@
 var http = require('http');
 var express = require('express');   
 var app = express();
-app.set('port', process.env.PORT || 8001);
+app.set('port', process.env.PORT || 8082);
 app.use(express.static(__dirname + '/app/'));
 
 app.get('/', function(req, res){
 res.send(' This is a test ');
 });
+app.get('/status', function(req, res){
+res.send('OK ');
+});
 
-var server = app.listen(8081, function() {
+var server = app.listen(8082, function() {
 console.log('Listening on port %d', server.address().port);
 console.log("Successfully loaded pearson My class");
 });
+
+
